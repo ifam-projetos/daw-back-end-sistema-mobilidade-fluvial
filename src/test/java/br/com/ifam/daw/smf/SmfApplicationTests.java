de5570df2d644f0eac25980fc6812af3ef284e5f
Otthon Leão <otthonleao@hotmail.com>
2024-03-11 09:26:23 -0400

    build: initial files from start initialize

---
+++ src/test/java/br/com/ifam/daw/smf/SmfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifam.daw.smf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
